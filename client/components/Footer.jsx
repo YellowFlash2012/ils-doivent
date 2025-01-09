7f9d743fb532fa05c8de8a8f3afee8b1dd97b565
YellowFlash2012 <cyclop9@hotmail.com>
2025-01-09 07:59:14 +0100

    customize the fotter by setting background-color and adding social icons.

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,13 +1,31 @@
 import { Col, Container, Row } from "react-bootstrap";
+import { FaInstagram } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { AiOutlineDiscord } from "react-icons/ai";
+import Button from "react-bootstrap/Button";
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
         <footer>
             <Container className="text-center">
+                <Row >
+                    <Col className="d-flex justify-content-center my-2">
+                        <FaInstagram className="social-icons" />
+                        <FaFacebookF className="mx-2 social-icons" />
+                        <FaLinkedinIn className="social-icons" />
+                        <FaXTwitter className="mx-2 social-icons" />
+                        <AiOutlineDiscord className="social-icons" />
+                    </Col>
+                </Row>
+
                 <Row>
                     <Col className="py-3">
-                        <p className="col-12">&copy; ils-doivent - {currentYear}</p>
+                        <p className="col-12">
+                            &copy; ils-doivent - {currentYear}
+                        </p>
                     </Col>
                 </Row>
             </Container>
